59211943eacb59159d65023c1f64c8a4b630ca84
phucvo <phucvoaff@gmail.com>
2023-05-05 22:09:35 +0700

    seperate folder microservices, added docker compose

--- set_env.sh
+++ set_env.sh
@@ -2,9 +2,9 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=Abc12345
-export POSTGRES_HOST=postgres-2.ci3lh7ol1uz9.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=postgres.ctm5t4oyaq1k.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::uda-project3-bushjdo
+export AWS_BUCKET=learning-udacity
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
--- udagram-api-feed/src/aws.ts
+++ udagram-api-feed/src/aws.ts
@@ -1,19 +1,18 @@
 import AWS = require('aws-sdk');
-import {config} from './config/config';
-
+import { config } from './config/config';
 
 // Configure AWS
-const credentials = new AWS.SharedIniFileCredentials({profile: config.aws_profile});
+const credentials = new AWS.SharedIniFileCredentials({ profile: config.aws_profile });
 AWS.config.credentials = credentials;
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
   region: config.aws_region,
-  params: {Bucket: config.aws_media_bucket},
+  params: { Bucket: config.aws_media_bucket },
 });
 
 // Generates an AWS signed URL for retrieving objects
-export function getGetSignedUrl( key: string ): string {
+export function getGetSignedUrl(key: string): string {
   const signedUrlExpireSeconds = 60 * 5;
 
   return s3.getSignedUrl('getObject', {
@@ -24,11 +23,12 @@ export function getGetSignedUrl( key: string ): string {
 }
 
 // Generates an AWS signed URL for uploading objects
-export function getPutSignedUrl( key: string ): string {
+export function getPutSignedUrl(key: string): string {
   const signedUrlExpireSeconds = 60 * 5;
-
+  // console.log('🚀 ~ file: aws.ts:35 ~ getPutSignedUrl ~ key:', key, config.aws_media_bucket);
   return s3.getSignedUrl('putObject', {
     Bucket: config.aws_media_bucket,
+    // Bucket: 'uda-p3-bushjdo',
     Key: key,
     Expires: signedUrlExpireSeconds,
   });
